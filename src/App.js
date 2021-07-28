3278820587981a534d6d60daaef21f8ac9e55674
Anthony Calderaro <ajc-sorin@protonmail.com>
2021-07-27 20:38:41 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const Header = () => {
+  return (
+    <div className='header'>
+      Battle Pet Flipbook
+    </div>
+  )
+}
+
+const PetCard = ({ pet }) => {
+  const servers = ['moon-guard', 'illidan', 'stormrage', 'area-52', 'bleeding-hollow'];
+
+  const { image, name, serverPrices } = pet;
+  return (
+    <div className='petcontainer'>
+      <div className='image'>{image}</div>
+      <div className='name'>{name}</div>
+      {servers.map(server => <div className='server'>{serverPrices[server]}</div>)}
+    </div>
+  )
+}
+const App = () => {
+  const [petData, setPetData] = useState([]);
+  const servers = ['moon-guard', 'illidan', 'stormrage', 'area-52', 'bleeding-hollow'];
+  useEffect(() => {
+    console.log('make query');
+    setPetData([
+      {
+        'image': 'image',
+        'name': 'Anubiseth Idol',
+        'serverPrices': {
+          'moon-guard': 1000,
+          'illidan': 2000,
+          'stormrage': 3000,
+          'area-52': 4000,
+          'bleeding-hollow': 5000,
+        }
+      },
+      {
+        'image': 'image',
+        'name': 'Chrominious',
+        'serverPrices': {
+          'moon-guard': 1000,
+          'illidan': 5000,
+          'stormrage': 4000,
+          'area-52': 3000,
+          'bleeding-hollow': 2000,
+        }
+      }
+    ])
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className='serverlist'>
+        {servers.map(server => <div >{server}</div>)}
+      </div>
+      {petData.map(pet => <PetCard pet={pet} />)}
     </div>
   );
 }
